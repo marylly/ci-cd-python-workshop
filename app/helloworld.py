e41f35e0010c62dad163e64ee798ae5d838cadb3
Marylly Araújo Silva <mymarylly@gmail.com>
2019-12-15 20:30:32 -0300

    Fix PyLint errors

--- app/helloworld.py
+++ app/helloworld.py
@@ -1,6 +1,6 @@
 """Hello World test application module"""
-import cherrypy
 import os
+import cherrypy
 
 class Helloworld():
     """Hello World class signature"""
@@ -15,8 +15,5 @@ class Helloworld():
         return "Bye bye World!"
 
 if __name__ == '__main__':
-    cherrypy.config.update({
-        'server.socket_host': '0.0.0.0',
-	    'server.socket_port': os.environ["PORT"]
-        })
+    cherrypy.config.update({'server.socket_host': '0.0.0.0', 'server.socket_port': os.environ["PORT"]})
     cherrypy.quickstart(Helloworld())
