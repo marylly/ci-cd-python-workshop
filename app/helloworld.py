f9b63c640dbb6bec43fe00775303c8e63daf5d36
Marylly Araújo Silva <mymarylly@gmail.com>
2019-12-15 22:11:55 -0300

    Black code style automatic fix

--- app/helloworld.py
+++ app/helloworld.py
@@ -2,8 +2,10 @@
 import os
 import cherrypy
 
-class Helloworld():
+
+class Helloworld:
     """Hello World class signature"""
+
     @cherrypy.expose
     def helloworld(self):
         """Hello world endpoint method."""
@@ -14,8 +16,10 @@ class Helloworld():
         """Bye bye world endpoint method."""
         return "Bye bye World!"
 
-if __name__ == '__main__':
-    cherrypy.config.update({'server.socket_host': '0.0.0.0',
-                            'server.socket_port': int(os.environ["PORT"])
-                           })
+
+if __name__ == "__main__":
+    cherrypy.config.update(
+        {"server.socket_host": "0.0.0.0",
+         "server.socket_port": int(os.environ["PORT"])}
+    )
     cherrypy.quickstart(Helloworld())
